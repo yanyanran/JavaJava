6c8c22e3276f384d3d7f5166061ea8cd52bb18ee
yan ran <93911762+yanyanran@users.noreply.github.com>
2022-04-28 21:51:09 +0800

    Add files via upload

---
+++ JAVA语言程序设计与数据结构/第十三章/test.java
@@ -0,0 +1,9 @@
+public class test {
+    public static void main(String[] args){
+        Number numberBuf = new Integer(0);
+        numberBuf = new Integer(1);
+        //Double doubleBuf = (Double)numberBuf;
+
+        System.out.println(numberBuf);
+    }
+}
