402971901adc00f661cef603cdfcead522868215
yan ran <93911762+yanyanran@users.noreply.github.com>
2022-05-28 21:50:11 +0800

    Add files via upload

---
+++ JAVA语言程序设计与数据结构/第二十一章/TestMethodsInCollection.java
@@ -0,0 +1,38 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class TestMethodsInCollection {
+    public static void main(String[] args){
+        Set<String> set1 = new HashSet<>();
+        set1.add("London");
+        set1.add("Paris");
+        set1.add("New York");
+        set1.add("Beijing");
+
+        System.out.println("set1 is " + set1);
+        System.out.println(set1.size() + " element in set1");
+
+        set1.remove("London");
+        System.out.println("\nset1 is " + set1);
+        System.out.println(set1.size() + " element in set1");
+
+        Set<String> set2 = new HashSet<>();
+
+        set2.add("London");
+        set2.add("shanghai");
+        set2.add("Paris");
+        System.out.println("\nset2 is " + set2);
+        System.out.println(set2.size() + " element in set2");
+
+        System.out.println("\nIs Taipei in set2 ?" + set2.contains("Taipei"));
+
+        set1.addAll(set2);
+        System.out.println("\nAfter adding set2 to set1, set1 is " + set1);
+
+        set1.removeAll(set2);
+        System.out.println("\nAfter removing set2 from set1,set1 is " + set1);
+
+        set1.retainAll(set2);
+        System.out.println("\nAfter retaining common elements in set2" + "and set2, set1 is " + set1);
+    }
+}
