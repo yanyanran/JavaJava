b9403d864762c1f5f3c1ca7a34650d46146dd36d
yanyanran <yanran66666@qq.com>
2022-06-15 23:45:52 +0800

    ThreadPool

--- 进程/MyThreadPool.java
+++ 进程/MyThreadPool.java
@@ -1,4 +1,9 @@
-public class MyThreadPool {
+import java.util.ArrayList;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
+
+public class MyThreadPool{
     private static BlockingQueueWithLock<Runnable> queue = null;
     private volatile int coreSize;
     private volatile int poolSize;  //当前线程数
@@ -6,9 +11,12 @@ public class MyThreadPool {
     private volatile int timeOut;
 
     public MyThreadPool(int corePoolSize, int maxPoolSize, int timeout) {
+        ExecutorService executor = Executors.newFixedThreadPool(3);
+        int num = ((ThreadPoolExecutor)executor).getActiveCount();
         this.coreSize = corePoolSize;
         this.maxSize = maxPoolSize;
         this.timeOut = timeout;
+
     }
 
     public void execute(Runnable task){
