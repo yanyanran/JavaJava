59c6f42078f9d1a88313ca7a4d1188d758064cf0
yanyanran <yanran66666@qq.com>
2022-06-19 11:28:31 +0800

    delete

--- JAVA语言程序设计与数据结构/第十章/Test5.java
+++ JAVA语言程序设计与数据结构/第十章/Test5.java
@@ -1,51 +1,5 @@
 import java.util.Scanner;
 
-public class StackOfIntegers {
-    private int[] elements;
-    private int size;
-    public static final int DEFAULT_CAPACITY = 16;
-
-    /** 构造一个默认容量为16的堆栈 */
-    public StackOfIntegers() {
-        this(DEFAULT_CAPACITY);
-    }
-
-    /** 构造具有指定容量的堆栈*/
-    public StackOfIntegers(int capacity) {
-        elements = new int[capacity];
-    }
-
-    /** 将新整数放入堆栈顶部 */
-    public void push(int value) {
-        if (size >= elements.length) {
-            int[] temp = new int[elements.length * 2];
-            System.arraycopy(elements, 0, temp, 0, elements.length);
-            elements = temp;
-        }
-        elements[size++] = value;
-    }
-
-    /**返回并从堆栈中移除顶部元素*/
-    public int pop() {
-        return elements[--size];
-    }
-
-    /** 从堆栈返回顶部元素 */
-    public int peek() {
-        return elements[size - 1];
-    }
-
-    /** 测试堆栈是否为空 */
-    public boolean empty() {
-        return size == 0;
-    }
-
-    /** 返回堆栈中的元素数 */
-    public int getSize() {
-        return size;
-    }
-}
-
 public class Test5 {
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
