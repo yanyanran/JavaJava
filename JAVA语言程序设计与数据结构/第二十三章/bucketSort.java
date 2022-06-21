73cc748259f774100f8f0386def4347129a1b100
yanyanran <yanran66666@qq.com>
2022-06-21 20:09:14 +0800

    bucketSort.java

--- JAVA语言程序设计与数据结构/第二十三章/bucketSort.java
+++ JAVA语言程序设计与数据结构/第二十三章/bucketSort.java
@@ -28,9 +28,29 @@ public class bucketSort {
         for (int i = 0; i < bucketArr.size(); i++) {
             Collections.sort(bucketArr.get(i));
         }
-
         System.out.println(bucketArr.toString());
+    }
 
+    public static void bucketSort(int[] a, int max) {
+        int[] buckets;
+
+        if (a==null || max<1)
+            return ;
+
+        // 创建一个容量为max的数组buckets，并且将buckets中的所有数据都初始化为0。
+        buckets = new int[max];
+
+        // 1. 计数
+        for(int i = 0; i < a.length; i++)
+            buckets[a[i]]++;
+
+        // 2. 排序
+        for (int i = 0, j = 0; i < max; i++) {
+            while( (buckets[i]--) >0 ) {
+                a[j++] = i;
+            }
+        }
+        buckets = null;
     }
 
     public static void main(String[] args) {
