e4fc3f122f1829e1020893da2a8e79c586c2ec30
yanyanran <yanran66666@qq.com>
2022-06-21 18:31:24 +0800

    bucketSort.java

---
+++ JAVA语言程序设计与数据结构/第二十三章/bucketSort.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class bucketSort {
+    public static void bucketSort(int[] arr) {
+
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+            max = Math.max(max, arr[i]);
+            min = Math.min(min, arr[i]);
+        }
+
+        //桶数
+        int bucketNum = (max - min) / arr.length + 1;
+        ArrayList<ArrayList<Integer>> bucketArr = new ArrayList<>(bucketNum);
+        for (int i = 0; i < bucketNum; i++) {
+            bucketArr.add(new ArrayList<Integer>());
+        }
+
+        //将每个元素放入桶
+        for (int i = 0; i < arr.length; i++) {
+            int num = (arr[i] - min) / (arr.length);
+            bucketArr.get(num).add(arr[i]);
+        }
+
+        //对每个桶进行排序
+        for (int i = 0; i < bucketArr.size(); i++) {
+            Collections.sort(bucketArr.get(i));
+        }
+
+        System.out.println(bucketArr.toString());
+
+    }
+
+    public static void main(String[] args) {
+        int[] list = {1, 0, 2, 2, 6, 4, 2, 3, -1, -4, -1};
+        bucketSort(list);
+    }
+}
