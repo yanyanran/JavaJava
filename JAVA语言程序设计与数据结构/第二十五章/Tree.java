49f7807239771d66d13fd77acd8015d18f1ff755
yanyanran <yanran66666@qq.com>
2022-06-26 22:29:20 +0800

    Tree接口

---
+++ JAVA语言程序设计与数据结构/第二十五章/Tree.java
@@ -0,0 +1,75 @@
+import java.util.Collection;
+
+public interface Tree<E> extends Collection<E> {
+    public boolean search(E e);
+
+    public boolean insert(E e);
+
+    public boolean delete(E e);
+
+    public int getSize();
+
+    public default void inorder(){
+    }
+
+    public default void postorder(){
+    }
+
+    public  default void preorder(){
+    }
+
+    @Override
+    public default boolean isEmpty(){
+        return size() == 0;
+    }
+
+    @Override
+    public default boolean contains(Object e){
+        return search((E)e);
+    }
+
+    @Override
+    public default boolean add(E e){
+        return insert(e);
+    }
+
+    @Override
+    public default boolean remove(Object e){
+        return delete((E)e);
+    }
+
+    @Override
+    public default int size(){
+        return getSize();
+    }
+
+    @Override
+    public default boolean containsAll(Collection<?> c){
+        return false;
+    }
+
+    @Override
+    public default boolean addAll(Collection<? extends E> c){
+        return false;
+    }
+
+    @Override
+    public default boolean removeAll(Collection<?> c){
+        return false;
+    }
+
+    @Override
+    public default boolean retainAll(Collection<?> c){
+        return false;
+    }
+
+    @Override
+    public default Object[] toArray(){
+        return null;
+    }
+
+    @Override
+    public default <T> T[] toArray(T[] array){
+        return null;
+    }
+}
