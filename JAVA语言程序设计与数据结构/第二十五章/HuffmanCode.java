c48caf77b0c33aa3dee364b2f8c28217da31ab30
yanyanran <yanran66666@qq.com>
2022-06-28 09:28:13 +0800

    HuffmanCode

--- JAVA语言程序设计与数据结构/第二十五章/HuffmanCode.java
+++ JAVA语言程序设计与数据结构/第二十五章/HuffmanCode.java
@@ -6,10 +6,12 @@ public class HuffmanCode {
         System.out.print("Enter text: ");
         String text = input.nextLine();
 
+        // 计算字符出现次数
         int[] counts = getCharacterFrequency(text);
 
         System.out.printf("%-15s%-15s%-15s%-15s\n","ASCII Code","Character","Frequency","Code");
 
+        // 基于counts得到哈夫曼树
         Tree tree = getHuffmanTree(counts);
         String[] codes = getCode(tree.root);
 
@@ -20,6 +22,7 @@ public class HuffmanCode {
         }
     }
 
+    // 获取每个叶子结点中字符的编码
     public static String[] getCode(Tree.Node root) {
         if(root == null){
             return null;
@@ -29,6 +32,7 @@ public class HuffmanCode {
         return codes;
     }
 
+    // 给树中每个结点赋予编码
     private static void assignCode(Tree.Node root, String[] codes){
         if(root.left != null){
             root.left.code = root.code + "0";
@@ -41,21 +45,26 @@ public class HuffmanCode {
         }
     }
 
+    // 返回一颗哈夫曼树
     public static Tree getHuffmanTree(int[] counts){
+        // 创建单结点树并添加到堆中
         Heap<Tree> heap = new Heap<>();
         for(int i = 0; i < counts.length; i++){
             if(counts[i] > 0){
                 heap.add(new Tree(counts[i],(char)i));
             }
         }
+
+        // while迭代
         while(heap.getSize() > 1){
-            Tree t1 = heap.remove();
+            Tree t1 = heap.remove(); //每次将权重最小的两个子树从堆中删除
             Tree t2 = heap.remove();
-            heap.add(new Tree(t1, t2));
+            heap.add(new Tree(t1, t2));  //t1,t2组合成新树。接着添加新树到堆中
         }
         return heap.remove();
     }
 
+    // 创建一个数组计算256个ASCII字符出现的次数
     public static int[] getCharacterFrequency(String text){
         int[] counts = new int[256];
 
@@ -103,8 +112,5 @@ public class HuffmanCode {
                 this.element = element;
             }
         }
-
     }
-
-
 }
