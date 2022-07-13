3c3a95668f8ec913cebb22ace9a61e8715cab4d6
yanyanran <yanran66666@qq.com>
2022-07-13 17:30:51 +0800

    Channel测试demo

--- Java网络编程/BIO和NIO/NIOServer.java
+++ Java网络编程/BIO和NIO/NIOServer.java
@@ -14,12 +14,13 @@ public class NIOServer {
         ServerSocketChannel serverSocketChannel = ServerSocketChannel.open();
         //2. 绑定对应的端口号
         serverSocketChannel.bind(new InetSocketAddress(9999));
-        //3. 通道默认是阻塞的，需要设置为非阻塞
-        // true 为通道阻塞false 为非阻塞
+        //3. 通道默认是阻塞的，需要设置为非阻塞（true 为通道阻塞，false 为非阻塞）
         serverSocketChannel.configureBlocking(false);
+
         System.out.println("服务端启动成功..........");
+
         while (true) {
-            //4. 检查是否有客户端连接 有客户端连接会返回对应的通道 , 否则返回null
+            //4. 检查是否有客户端连接（有返回对应的通道 , 否则返回null）
             SocketChannel socketChannel = serverSocketChannel.accept();
             if (socketChannel == null) {
                 System.out.println("没有客户端连接...我去做别的事情");
@@ -28,16 +29,16 @@ public class NIOServer {
             }
             //5. 获取客户端传递过来的数据,并把数据放在byteBuffer这个缓冲区中
             ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
-            //返回值:
-            //正数: 表示本次读到的有效字节个数.
-            //0: 表示本次没有读到有效字节.
-            //-1: 表示读到了末尾
+            /**
+             * 返回值:
+             * 正数: 表示本次读到的有效字节个数.
+             * 0: 表示本次没有读到有效字节.
+             * -1: 表示读到了末尾
+             **/
             int read = socketChannel.read(byteBuffer);
-            System.out.println("客户端消息:" +
-                    new String(byteBuffer.array(), 0, read,
-                            StandardCharsets.UTF_8));
+            System.out.println("客户端消息:" + new String(byteBuffer.array(), 0, read,StandardCharsets.UTF_8));
             //6. 给客户端回写数据
-            socketChannel.write(ByteBuffer.wrap("没钱".getBytes(StandardCharsets.UTF_8)));
+            socketChannel.write(ByteBuffer.wrap("知道拉！".getBytes(StandardCharsets.UTF_8)));
             //7. 释放资源
             socketChannel.close();
         }
