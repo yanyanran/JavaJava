12ffeab8e863db2361588683dc6685d86ed972bc
yanyanran <yanran66666@qq.com>
2022-07-13 20:01:46 +0800

    Selector测试demo

--- Java网络编程/BIO和NIO/NIOSelectorServer.java
+++ Java网络编程/BIO和NIO/NIOSelectorServer.java
@@ -26,38 +26,46 @@ public class NIOSelectorServer {
         Selector selector = Selector.open();
         //5. 将服务端通道注册到选择器上,并指定注册监听的事件为OP_ACCEPT
         serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT);
+
         System.out.println("服务端启动成功...");
+
         while (true) {
             //6. 检查选择器是否有事件
             int select = selector.select(2000);
             if (select == 0) {
                 continue;
             }
+
             //7. 获取事件集合
             Set<SelectionKey> selectionKeys = selector.selectedKeys();
+            // 迭代器重出江湖！！！
             Iterator<SelectionKey> iterator = selectionKeys.iterator();
+
             while (iterator.hasNext()) {
-                //8. 判断事件是否是客户端连接事件SelectionKey.isAcceptable()
                 SelectionKey key = iterator.next();
-                //9. 得到客户端通道,并将通道注册到选择器上, 并指定监听事件为OP_READ
+
+                //8. 判断事件是否是客户端连接事件 --> SelectionKey.isAcceptable()
                 if (key.isAcceptable()) {
+                    //9. 得到客户端通道,并将通道注册到选择器上
                     SocketChannel socketChannel = serverSocketChannel.accept();
                     System.out.println("客户端已连接......" + socketChannel);
+
                     //必须设置通道为非阻塞, 因为selector需要轮询监听每个通道的事件
                     socketChannel.configureBlocking(false);
                     //并指定监听事件为OP_READ
                     socketChannel.register(selector, SelectionKey.OP_READ);
                 }
-                //10. 判断是否是客户端读就绪事件SelectionKey.isReadable()
+
+                //10. 判断是否是客户端读就绪事件 --> SelectionKey.isReadable()
                 if (key.isReadable()) {
-                    //11.得到客户端通道,读取数据到缓冲区
+                    //11.得到客户端通道
                     SocketChannel socketChannel = (SocketChannel) key.channel();
                     ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
-                    int read = socketChannel.read(byteBuffer);
+                    int read = socketChannel.read(byteBuffer); // 读取数据到缓冲区
                     if (read > 0) {
                         System.out.println("客户端消息:" + new String(byteBuffer.array(), 0, read, StandardCharsets.UTF_8));
                         //12.给客户端回写数据
-                        socketChannel.write(ByteBuffer.wrap("没钱".getBytes(StandardCharsets.UTF_8)));
+                        socketChannel.write(ByteBuffer.wrap("知道拉！".getBytes(StandardCharsets.UTF_8)));
                         socketChannel.close();
                     }
                 }
