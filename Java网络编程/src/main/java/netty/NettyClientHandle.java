ecd61c3c0fe9f366c655efd67eddf32af93cf1ec
yanyanran <yanran66666@qq.com>
2022-07-15 11:11:48 +0800

    channelHander

--- Java网络编程/src/main/java/netty/NettyClientHandle.java
+++ Java网络编程/src/main/java/netty/NettyClientHandle.java
@@ -17,19 +17,17 @@ public class NettyClientHandle implements ChannelInboundHandler {
      */
     @Override
     public void channelActive(ChannelHandlerContext ctx) throws Exception {
-        ctx.writeAndFlush(Unpooled.copiedBuffer("你好呀,我是Netty客户端",
-                CharsetUtil.UTF_8));
+        ctx.writeAndFlush(Unpooled.copiedBuffer("你好呀,我是Netty客户端", CharsetUtil.UTF_8));
     }
+
     @Override
-    public void channelRead(ChannelHandlerContext ctx, Object msg) throws
-            Exception {
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
         ByteBuf byteBuf = (ByteBuf) msg;
-        System.out.println("服务端发来消息:" +
-                byteBuf.toString(CharsetUtil.UTF_8));
+        System.out.println("服务端发来消息:" + byteBuf.toString(CharsetUtil.UTF_8));
     }
+
     @Override
-    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause)
-            throws Exception {
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
     }
     @Override
     public void channelRegistered(ChannelHandlerContext ctx) throws Exception {
--- Java网络编程/src/main/java/netty/NettyServerHandle.java
+++ Java网络编程/src/main/java/netty/NettyServerHandle.java
@@ -15,12 +15,12 @@ public class NettyServerHandle implements ChannelInboundHandler {
      * @throws Exception
      */
     @Override
-    public void channelRead(ChannelHandlerContext ctx, Object msg) throws
-            Exception {
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
         ByteBuf byteBuf = (ByteBuf) msg;
         System.out.println("客户端发来消息:" +
                 byteBuf.toString(CharsetUtil.UTF_8));
     }
+
     /**
      * 读取完毕事件
      *
@@ -29,9 +29,11 @@ public class NettyServerHandle implements ChannelInboundHandler {
      */
     @Override
     public void channelReadComplete(ChannelHandlerContext ctx) throws Exception
-    {ctx.writeAndFlush(Unpooled.copiedBuffer("你好,我是Netty服务端.",
+    {
+        ctx.writeAndFlush(Unpooled.copiedBuffer("你好,我是Netty服务端.",
             CharsetUtil.UTF_8));
     }
+
     /**
      * 异常发生事件
      *
@@ -40,11 +42,11 @@ public class NettyServerHandle implements ChannelInboundHandler {
      * @throws Exception
      */
     @Override
-    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause)
-            throws Exception {
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
         cause.printStackTrace();
         ctx.close();
     }
+
     @Override
     public void channelRegistered(ChannelHandlerContext ctx) throws Exception {
     }
@@ -52,6 +54,7 @@ public class NettyServerHandle implements ChannelInboundHandler {
     public void channelUnregistered(ChannelHandlerContext ctx) throws Exception
     {
     }
+
     /**
      * 通道就绪事件
      *
@@ -61,16 +64,15 @@ public class NettyServerHandle implements ChannelInboundHandler {
     @Override
     public void channelActive(ChannelHandlerContext ctx) throws Exception {
     }
+
     @Override
     public void channelInactive(ChannelHandlerContext ctx) throws Exception {
     }
     @Override
-    public void userEventTriggered(ChannelHandlerContext ctx, Object evt) throws
-            Exception {
+    public void userEventTriggered(ChannelHandlerContext ctx, Object evt) throws Exception {
     }
     @Override
-    public void channelWritabilityChanged(ChannelHandlerContext ctx) throws
-            Exception {
+    public void channelWritabilityChanged(ChannelHandlerContext ctx) throws Exception {
     }@Override
     public void handlerAdded(ChannelHandlerContext ctx) throws Exception {
     }
